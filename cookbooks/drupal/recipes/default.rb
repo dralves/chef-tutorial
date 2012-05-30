2acd08d9534119fad39da6301819fe4a3e6a6e39
David Ribeiro Alves <davidralves@gmail.com>
2012-05-30 05:42:29 +0100

    corrected drupal recipe

--- cookbooks/drupal/recipes/default.rb
+++ cookbooks/drupal/recipes/default.rb
@@ -44,7 +44,7 @@ template "/etc/mysql/drupal-grants.sql" do
 end
 
 Gem.clear_paths
-require 'mysql'
+require 'mysql_api'
 
 execute "create #{node[:drupal][:db][:database]} database" do
   command "/usr/bin/mysqladmin -u root -p#{node[:mysql][:server_root_password]} create #{node[:drupal][:db][:database]}"
